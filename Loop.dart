430701cf4bc5c2ff66c6b2cb2f85e84a6e1ea9a7
jazib-ahmed <jazib.best@gmail.com>
2023-09-07 14:22:57 +0500

    Loop sir code

---
+++ Loop.dart
@@ -0,0 +1,119 @@
+void main() {
+  // Nested for loop - Star Patterns
+
+  // *
+  // **
+  // ***
+  // ****
+  // *****
+
+  // for (var i = 1; i <= 5; i++) {
+  //   for (var j = 1; j <= i; j++) {
+  //    stdout.write("*");
+  //   }
+  //   print("");
+  // }
+
+  // *****
+  // ****
+  // ***
+  // **
+  // *
+
+  // for (var i = 5; i >= 1; i--) {
+  //   for (var j = 1; j <= i; j++) {
+  //    stdout.write("*");
+  //   }
+  //   print("");
+  // }
+
+  // 1
+  // 12
+  // 123
+  // 1234
+  // 12345
+
+  // for (var i = 1; i <= 5; i++) {
+  //   for (var j = 1; j <= i; j++) {
+  //    stdout.write("$j");
+  //   }
+  //   print("");
+  // }
+
+  // 1
+  // 22
+  // 333
+  // 4444
+  // 55555
+
+  // for (var i = 1; i <= 5; i++) {
+  //   for (var j = 1; j <= i; j++) {
+  //    stdout.write("$i");
+  //   }
+  //   print("");
+  // }
+
+  // 1
+  // 23
+  // 456
+  // 78910
+
+  // var count = 1;
+  // for (var i = 1; i <= 4; i++) {
+  //   for (var j = 1; j <= i; j++) {
+  //    stdout.write("$count");
+  //    count++;
+  //   }
+  //   print("");
+  // }
+
+// While Loop with examples
+
+  // while (condition) {
+  //   // code to be executed
+  // }
+
+  // int i = 0;
+  // while (i < 10) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // int i = 10;
+  // while (i > 0) {
+  //   print(i);
+  //   i--;
+  // }
+
+  // login with input email & password - try again if user enters wrong email or password
+  // String email = stdin.readLineSync();
+  // String password = stdin.readLineSync();
+  // bool isLogin = false;
+  // while (!isLogin) {
+  //   if (email == "bilal@gmail" && password == "123456") {
+  //     print("Login Successfull");
+  //     isLogin = true;
+  //   } else {
+  //     print("Please enter correct email & password");
+  //     email = stdin.readLineSync();
+  //     password = stdin.readLineSync();
+  //   }
+  // }
+
+  // do while loop
+  // do {
+  //   // code to be executed
+  // } while (condition);
+
+  // int i = 0;
+  // do {
+  //   print(i);
+  //   i++;
+  // } while (i < 10);
+
+  // int i = 10;
+  // do {
+  //   print(i);
+  //   i--;
+  // } while (i > 0);
+}
